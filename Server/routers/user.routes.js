77174dfd4670c7fd103dbc7a2bcc62178f5352c9
kajalsingh748927-debug <kajalsingh748927@gmail.com>
2026-02-17 20:36:35 +0530

    Fix import path case sensitivity and clean up routes

--- Server/routers/user.routes.js
+++ Server/routers/user.routes.js
@@ -1,4 +1,5 @@
 import express from "express";
+// CHANGE 'controllers' to 'Controllers' to match your folder name!
 import {
   getAllProducts,
   getProductById,
@@ -7,19 +8,12 @@ import {
   cancelOrder,
   getVendorProducts,
   getVendors
-} from "../controllers/user.controller.js";
+} from "../Controllers/user.controller.js"; 
 
 import { protect } from "../middleware/auth.middleware.js";
 import { isUser } from "../middleware/role.middleware.js";
 
 const router = express.Router();
-
-// 🟢 TEST ROUTE (VERIFY FIRST)
-router.get("/test", (req, res) => {
-  res.send("USER ROUTES WORKING");
-});
-
-// 🟢 GET ALL PRODUCTS (THIS FIXES YOUR ERROR)
 
 // GET SINGLE PRODUCT
 router.get("/product/:id", protect, getProductById);
@@ -30,12 +24,10 @@ router.get("/products", protect, getAllProducts);
 // GET VENDOR PRODUCTS
 router.get("/products/vendor/:vendorId", protect, getVendorProducts);
 
-
 // Orders
 router.post("/order", protect, isUser, placeOrder);
 router.get("/orders", protect, isUser, getMyOrders);
 router.put("/order/:id/cancel", protect, isUser, cancelOrder);
 router.get('/vendors', getVendors);
-
 
 export default router;
